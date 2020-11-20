70f4663015931866d92102c935db06346c85c80d
Jeff Ng <jeff-ng@hotmail.com>
2020-11-20 12:10:55 +0800

    setup app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray, FormGroupDirective } from '@angular/forms';
+import { v4 as uuidv4 } from 'uuid';
+import { orderClass } from './orderClass';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mockAssess';
 }
